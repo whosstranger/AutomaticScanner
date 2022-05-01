ff910ea9c94529ebe3bee0de63a3db9878edadba
whosstranger <cristian_fer.arango@uao.edu.co>
2022-05-01 05:38:43 +0000

    Automatic Scanner

--- automatic-scanner.py
+++ automatic-scanner.py
@@ -1,5 +1,4 @@
-import sys, pyfiglet, csv, wfuzz, signal
-import nmap
+import sys, pyfiglet, csv, wfuzz, signal, nmap, os
 
 def def_handler(sig, frame):
     print("\n\n[!] Going out...\n")
@@ -24,15 +23,25 @@ ep = nmap.PortScanner()
 
 if option == "1": #Scan ports        
     ip = input("Place the IP: ")
-    print("Scanning...")
+    print("\n Scanning... \n")
     ep.scan(hosts = ip, arguments = '-p- --open --min-rate 5000 -n -Pn')
-    print("Command executed: " + ep.command_line()) #Showing the command
+    print("Command executed: " + ep.command_line() + '\n') #Showing the command
     for host in ep.all_hosts():
         for proto in ep[host].all_protocols():
             lport = ep[host][proto].keys()
             sorted(lport)
             for port in lport:
+                print("-----------------------")
                 print ('Port: %s  State: %s' % (port, ep[host][proto][port]['state']))
+                print("-----------------------")
+    info = input("\nDo you want to safe the information? (y/n): ")
+    if info == "y":
+        with open("Ports.txt","w") as file:
+            file.write("Port: " + str(port)) 
+            file.write("   State: open")
+        print("Saved information")
+    elif info == "n":
+        print("\n [!] Going out...")
 elif option == "2": #Services
     a = input("Place the IP: ")
     print("Scanning...")
@@ -57,4 +66,11 @@ elif option == "4": #Help
                     --min-rate 5000: Number of sent threads
                     -n: Do not apply dns resolution
                     -Pn: Do not perform dns discovery
+                    """)
+    elif help == "2":
+        print("""Quick and easy command:
+                    hc: Dont show the status code. 
+                    
+
+
                     """)
